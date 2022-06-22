1f18554352fbf8db93dfd4a911582890d344803d
Rudi Butarbutar <89676713+rubutar@users.noreply.github.com>
2022-06-22 16:51:37 +0700

    retirement1.1

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct _retire_info
+{
+    int months;
+    double contribution;
+    double rate_of_return;
+};
+typedef struct _retire_info retire_info;
+
+void    retirement (int startAge, //in months
+                    double initial, //initial saving in dollars
+                    retire_info working, //info about working
+                    retire_info retired) //info about being retired
+
+
